43f4c2a12f70170823a9aad54074a90289572e3c
ryuhyewon <ryuhyewon74@gmail.com>
2020-03-17 23:03:30 +0900

    first commit

---
+++ memo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memo
+//
+//  Created by 60058231 on 17/03/2020.
+//  Copyright © 2020 60058231. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
